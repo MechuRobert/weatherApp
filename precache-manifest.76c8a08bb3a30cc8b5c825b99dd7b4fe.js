2db09d0535a34b7c307dbdbbf5fbfa4160e4649a
Mechu <mehringrobert96@gmail.com>
2020-04-29 15:13:10 +0200

    Updates

---
+++ precache-manifest.76c8a08bb3a30cc8b5c825b99dd7b4fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a107ae93a69ccbd8cc989d8277b7dd53",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "cafd50a3fc954994bb8e",
+    "url": "/weatherApp/static/css/main.57dc2430.chunk.css"
+  },
+  {
+    "revision": "46283107496a811f1b73",
+    "url": "/weatherApp/static/js/2.8530d25f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherApp/static/js/2.8530d25f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cafd50a3fc954994bb8e",
+    "url": "/weatherApp/static/js/main.3fb45915.chunk.js"
+  },
+  {
+    "revision": "3c29b40e68cf9091ee60",
+    "url": "/weatherApp/static/js/runtime-main.bd688866.js"
+  }
+]);
